df00c4ca29756fcf7ac9ddd43533eb70a288fe67
Bruno Soares da Silva <brunosoares.inf@gmail.com>
2015-07-02 02:01:27 -0300

    Correção de lock permanente para envio de mensagens de clientes remotos;

--- src/sending/MessageSender.java
+++ src/sending/MessageSender.java
@@ -62,18 +62,18 @@ public class MessageSender implements Runnable, MessageSenderObservable {
 				throw new IOException();
 			}
 			
-			//Waits until the client is able to receive messages;
-			while(!messagingHeader.getDestinations()[0].isReadyToReceive()){
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException ex) {
-					System.out.println("Error waiting the client be ready "
-							+ "to receive message: " + ex.getMessage());
-				}
-			}
-			
 			Socket socket = new Socket();
 			if (destinationMUIA.getName().equals(Main.getSelf().getName())) {
+				//Waits until the client is able to receive messages;
+				while(!messagingHeader.getDestinations()[0].isReadyToReceive()){
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException ex) {
+						System.out.println("Error waiting the client be ready "
+								+ "to receive message: " + ex.getMessage());
+					}
+				}
+				
 				socket.connect(new InetSocketAddress(
 						messagingHeader.getDestinations()[0].getAddress(),
 						messagingHeader.getDestinations()[0].getPort()),
